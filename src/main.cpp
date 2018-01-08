7df56b794f4fb5a154f7478f2c55d566312ed51c
Adrian Johnson <adrian@cqdev.co.uk>
2018-01-08 16:17:45 +0000

    config shifted to separate header, renaming of timer, fixed notes, wavetable header added

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,40 @@
-#include <Arduino.h>
-#include "notes.h"
+#include "wavetable.h"
 
-#define TICKS_PER_SECOND 192e3
+HardwareTimer timer(1);
 
-HardwareTimer fastTimer(1);
+uint tickCount = 0;
+uint halfTicksPerSecond = ticks_per_second * 0.5;
 
-const uint8 pins[] = { PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7 };
-//const uint8 pins[] = { PA7, PA6, PA5, PA4, PA3, PA2, PA1, PA0 };
+void tick() {
+	if (tickCount == ticks_per_second) {
+		tickCount = 0;
+	}
 
-void fastTick() {
+	if (tickCount == 0) {
+		digitalWrite(pin_led, LOW);
+	}
 
+	if (tickCount == halfTicksPerSecond) {
+		digitalWrite(pin_led, HIGH);
+	}
+
+	tickCount++;
 }
 
 void setup() {
-	//pinMode(PIN_OUT, OUTPUT);
+	pinMode(pin_led, OUTPUT);
 
-	fastTimer.pause();
-	fastTimer.setPeriod(1e6 / TICKS_PER_SECOND);
-	fastTimer.attachCompare1Interrupt(fastTick);
-	fastTimer.refresh();
-	fastTimer.resume();
+	for (int i = 0; i < bits; i++) {
+		pinMode(pins[i], OUTPUT);
+	}
 
+	calculateWavetable();
 
+	timer.pause();
+	timer.setPeriod(1e6 / ticks_per_second);
+	timer.attachCompare1Interrupt(tick);
+	timer.refresh();
+	timer.resume();
 }
 
 void loop() {
---
+++ src/wavetable.h
@@ -0,0 +1,21 @@
+#include "config.h"
+#include "notes.h"
+#include "math.h"
+
+const uint8 bits = sizeof(pins) / sizeof(uint8);
+const uint16 amplitudes = 2 ^ bits;
+
+uint8 sine[phases] = {};
+
+void calculateWavetable() {
+	uint16 halfAmplitudes = amplitudes / 2;
+	double radiansPerPhase = (2 * PI) / phases;
+
+	for (uint32 i = 0; i < phases; i++) {
+		double value = sin(i * radiansPerPhase);
+		value = value * halfAmplitudes;
+		value += halfAmplitudes;
+
+		sine[i] = round(value);
+	}
+}
